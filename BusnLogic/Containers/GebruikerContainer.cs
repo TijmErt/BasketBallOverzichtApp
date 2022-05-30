d0ccf01f7affec1dfbddccea6fb5dc2245d22372
Tijm van Dongen <486333@student.fontys.nl>
2022-05-30 15:09:50 +0200

    -summary's voor Gebruiker laag af

--- BusnLogic/Containers/GebruikerContainer.cs
+++ BusnLogic/Containers/GebruikerContainer.cs
@@ -22,6 +22,12 @@ namespace BusnLogic.Containers
             Container = container;
         }
 
+        /// <summary>
+        /// Hier word een gerbuiker terug gegeven als je een email mee geeft
+        /// </summary>
+        /// <param name="Email"></param>
+        /// <returns>er word een gerbuikerDTO terug gegegeven</returns>
+        /// <exception cref="Exception"></exception>
         public Gebruiker GetGebruiker(string Email)
         {
             GebruikerDTO dto = Container.GetGebruiker(Email);
@@ -29,10 +35,10 @@ namespace BusnLogic.Containers
         }
 
         /// <summary>
-        /// 
+        /// Hier mee haal je ale gebruikers op die in een team zitten
         /// </summary>
-        /// <param name="TeamID"></param>
-        /// <returns></returns>
+        /// <param name="TeamID">Geef hier het ID van de Team mee</param>
+        /// <returns>Het geeft een lijst van Gebruikers die in de gegeven team zitten</returns>
         public List<Gebruiker> GetGebruikerFromTeam(int TeamID)
         {
             List<Gebruiker> list = new List<Gebruiker>();
@@ -47,7 +53,7 @@ namespace BusnLogic.Containers
         /// Hier mee haal je alle leden van een specifieke club op.
         /// </summary>
         /// <param name="ClubID">hier moet de ID van de club in komen</param>
-        /// <returns></returns>
+        /// <returns>Het geeft een lijst van gebruikers terug die in de gegeven club zitten</returns>
         public List<Gebruiker> GetAllFromClub(int ClubID)
         {
             List<Gebruiker> list = new List<Gebruiker>();
@@ -61,14 +67,20 @@ namespace BusnLogic.Containers
         /// <summary>
         /// Hier mee wordt er een account voor een gebruiker gemaakt in de database
         /// </summary>
-        /// <param name="dto">geef hier een variable mee van het type gebruikerDTO</param>
-        /// <param name="wachtwoord" >Geef hier de een string mee voor het wachtwoord van de gebruiker</param>
+        /// <param name="gebruiker">geef hier een variable mee van het type gebruikerDTO</param>
+        /// <param name="wachtwoord">Geef hier de een string mee voor het wachtwoord van de gebruiker</param>
         public void CreateGebruikerAccount(Gebruiker gebruiker, string wachtwoord)
         {
             GebruikerDTO dto = gebruiker.GetDTO();
             Container.CreateGebruikerAccount(dto, wachtwoord);
         }
 
+        /// <summary>
+        /// Hier word er met een account ingelogd door te kijken of er een email is die overeen komt met het wachtwoord
+        /// </summary>
+        /// <param name="Email">Hier word de email van de gebruiker ingevoerd</param>
+        /// <param name="wachtwoord">Hier word het wachtwoord van de gebruiker ingevoerd</param>
+        /// <returns>Het geeft en GebruikerDTO terug</returns>
         public Gebruiker FindByEmailAndPassWordkGebruiker(string Email, string wachtwoord)
         {
             GebruikerDTO dto = Container.FindByEmailAndPassWordkGebruiker(Email, wachtwoord);
@@ -79,14 +91,23 @@ namespace BusnLogic.Containers
             return new Gebruiker(dto);
         }
 
-        public void InsertGebruikerInToTeam(int SpelerID, int TeamID)
+        /// <summary>
+        /// Hier mee zet je een gebruiker in een Team
+        /// </summary>
+        /// <param name="GebruikerID">Geef hier de Gebruiker ID mee</param>
+        /// <param name="TeamID">Geef hier de Team ID mee</param>
+        public void InsertGebruikerInToTeam(int GebruikerID, int TeamID)
         {
-            Container.InsertGebruikerInToTeam(SpelerID, TeamID);
+            Container.InsertGebruikerInToTeam(GebruikerID, TeamID);
         }
 
-        public void RemoveSpelerFromTeam(int SpelerID)
+        /// <summary>
+        /// Hier mee haal je een Gebruiker uit een team
+        /// </summary>
+        /// <param name="GebruikerID">Geef hier de ID mee van de speler die er uit moet</param>
+        public void RemoveSpelerFromTeam(int GebruikerID)
         {
-            Container.RemoveGebruikerFromTeam(SpelerID);
+            Container.RemoveGebruikerFromTeam(GebruikerID);
         }
     }
 }
--- InterfaceLib/InterFaces/IGebruikerContainer.cs
+++ InterfaceLib/InterFaces/IGebruikerContainer.cs
@@ -9,20 +9,54 @@ namespace InterfaceLib.InterFaces
 {
     public interface IGebruikerContainer
     {
+        /// <summary>
+        /// Hier word er met een account ingelogd door te kijken of er een email is die overeen komt met het wachtwoord
+        /// </summary>
+        /// <param name="Email">Hier word de email van de gebruiker ingevoerd</param>
+        /// <param name="wachtwoord">Hier word het wachtwoord van de gebruiker ingevoerd</param>
+        /// <returns>Het geeft en GebruikerDTO terug</returns>
         public GebruikerDTO FindByEmailAndPassWordkGebruiker(string Email, string wachtwoord);
+
+        /// <summary>
+        /// Hier word een gerbuiker terug gegeven als je een email mee geeft
+        /// </summary>
+        /// <param name="Email"></param>
+        /// <returns>er word een gerbuikerDTO terug gegegeven</returns>
+        /// <exception cref="Exception"></exception>
         public GebruikerDTO GetGebruiker(string Email);
+
+        /// <summary>
+        /// Hier mee haal je alle leden van een specifieke club op.
+        /// </summary>
+        /// <param name="ClubID">hier moet de ID van de club in komen</param>
+        /// <returns>Het geeft een lijst van gebruikers terug die in de gegeven club zitten</returns>
         public List<GebruikerDTO> GetAllFromClub(int ClubID);
+
         /// <summary>
         /// Hier mee wordt er een account voor een gebruiker gemaakt in de database
         /// </summary>
         /// <param name="dto">geef hier een variable mee van het type gebruikerDTO</param>
-        /// <param name="wachtwoord" >Geef hier de een string mee voor het wachtwoord van de gebruiker</param>
+        /// <param name="wachtwoord">Geef hier de een string mee voor het wachtwoord van de gebruiker</param>
         public void CreateGebruikerAccount(GebruikerDTO dto, string wachtwoord);
 
+        /// <summary>
+        /// Hier mee haal je ale gebruikers op die in een team zitten
+        /// </summary>
+        /// <param name="TeamID">Geef hier het ID van de Team mee</param>
+        /// <returns>Het geeft een lijst van Gebruikers die in de gegeven team zitten</returns>
         public List<GebruikerDTO> GetGebruikerFromTeam(int TeamID);
 
-        public void InsertGebruikerInToTeam(int SpelerID, int TeamID);
+        /// <summary>
+        /// Hier mee zet je een gebruiker in een Team
+        /// </summary>
+        /// <param name="GebruikerID">Geef hier de Gebruiker ID mee</param>
+        /// <param name="TeamID">Geef hier de Team ID mee</param>
+        public void InsertGebruikerInToTeam(int GebruikerID, int TeamID);
 
-        public void RemoveGebruikerFromTeam(int SpelerID);
+        /// <summary>
+        /// Hier mee haal je een Gebruiker uit een team
+        /// </summary>
+        /// <param name="GebruikerID">Geef hier de ID mee van de speler die er uit moet</param>
+        public void RemoveGebruikerFromTeam(int GebruikerID);
     }
 }
