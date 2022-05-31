c9e02572a3bb931f52fde058861d6a3922db6366
Tijm van Dongen <486333@student.fontys.nl>
2022-05-31 15:27:01 +0200

    -Wedstrijden laag toegevoegd

---
+++ BusnLogic/Containers/WedstrijdenConainer.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceLib.InterFaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusnLogic.Containers
+{
+    public class WedstrijdenConainer
+    {
+        private readonly IWedstrijdContainer container;
+
+        public WedstrijdenConainer(IWedstrijdContainer container)
+        {
+            this.container = container;
+
+        }
+    }
+}
